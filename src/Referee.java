b366b0bcb1c3d60c88370c897486aa552eec7ff8
harlan.howe <harlan.howe@kinkaid.org>
2021-09-20 15:22:05 -0500

    Constructor in Referee

--- src/Referee.java
+++ src/Referee.java
@@ -4,6 +4,11 @@ public class Referee
     private CardPile deck;
     private Player player1, player2;
 
-
+    public Referee()
+    {
+        whoseTurn = 1;
+        deck = CardPile.makeDeck(CardPile.ACE_HIGH);
+        System.out.println("Made deck with "+deck.size()+" cards.");
+    }
 }
 
