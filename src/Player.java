7f902a427188f190c419797aed59ce812853e072
harlan.howe <harlan.howe@kinkaid.org>
2021-09-22 14:23:55 -0500

    Created Player class with stub functions

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,40 @@
-public class Player {
+public class Player
+{
+    private CardPile myCards;
+    private String name;
+
+
+    public Player()
+    {
+
+
+    }
+
+    public void takeCardsAndShuffle(CardPile cardsToTake)
+    {
+
+
+    }
+
+    public Card getTopCard()
+    {
+
+        return null; // temporary code
+    }
+
+    public CardPile get3Cards()
+    {
+
+        return null; // temporary code
+    }
+
+    public int getNumCards()
+    {
+        return -1;  // temporary code
+    }
+
+    public String getName()
+    {
+        return null; // temporary code
+    }
 }
