862fc72a77eb9b3bbbfd3dead1a1fa497735f3e9
harlan.howe <harlan.howe@kinkaid.org>
2021-09-22 14:31:05 -0500

    Commented stub functions

--- src/Player.java
+++ src/Player.java
@@ -10,24 +10,43 @@ public class Player
 
     }
 
+    /**
+     * shuffles up the cards in the cardsToTake and adds all of them
+     * to myCards.
+     * @param cardsToTake - the pile of cards to add to my hand
+     */
     public void takeCardsAndShuffle(CardPile cardsToTake)
     {
 
 
     }
 
+    /**
+     * remove the first card from myCards and return it.
+     * @return - the card that was previously on the top of the pile.
+     */
     public Card getTopCard()
     {
 
         return null; // temporary code
     }
 
+    /**
+     * remove first 3 cards from myCards, put them into a new CardPile
+     * and return that card pile. (If we have three cards... if not,
+     * give what you can.)
+     * @return - a pile of 3 cards
+     */
     public CardPile get3Cards()
     {
 
         return null; // temporary code
     }
 
+    /**
+     * count how many cards are in myCards
+     * @return - the number of cards.
+     */
     public int getNumCards()
     {
         return -1;  // temporary code
