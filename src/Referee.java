5b1e8d17d0d99a34f6f2bda72e2dc5e5e05fb06b
harlan.howe <harlan.howe@kinkaid.org>
2021-09-22 14:35:13 -0500

    Created variables and stub functions

--- src/Referee.java
+++ src/Referee.java
@@ -1,7 +1,7 @@
 public class Referee
 {
     private int whoseTurn;
-    private CardPile deck;
+    private CardPile deck, pool;
     private Player player1, player2;
 
     public Referee()
@@ -9,6 +9,23 @@ public class Referee
         whoseTurn = 1;
         deck = CardPile.makeDeck(CardPile.ACE_HIGH);
         System.out.println("Made deck with "+deck.size()+" cards.");
+    }
+
+    public void dealCards()
+    {
+
+
+    }
+
+    public void resetGame()
+    {
+
+
+    }
+
+    public void playGame()
+    {
+
     }
 }
 
